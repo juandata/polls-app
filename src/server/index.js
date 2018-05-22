3d70c2eb9de626c90f101b45774b9c54a0e64614
juandata <juandavidtabaresarce@gmail.com>
2018-05-22 13:08:59 -0400

    from dell

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const os = require('os');
-
 const app = express();
 
 app.use(express.static('dist'));
+app.get("/other", function(req, res){
+  //res.sendFile("../dist/index.html");
+  res.send("hello soy other");
+});
 app.get('/api/getUsername', (req, res) => res.send({ username: "Hola soy express" }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
