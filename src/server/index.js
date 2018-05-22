a57af6c26f9f568dcde03233bedc16450c0c7168
juandata <juandavidtabaresarce@gmail.com>
2018-05-22 19:11:27 -0400

    uploading fromd dell

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path  = require("path");
 const app = express();
+/*
+app.get('/*', function(req, res) {
+  //app.use(express.static('../../dist'));
+  res.sendFile(express.static('../../dist'));
 
-app.use(express.static('dist'));
+})*/
+/*
+app.get("/", function(req, res){
+  //res.send("express working");
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});*/
+app.use(express.static('../../dist'));
+app.get("/other", function(req, res){
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
+app.get("/signup", function(req, res){
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 app.get('/api/getUsername', (req, res) => res.send({ username: "Hola soy express" }));
 /*app.get('/', (req, res){
   res.send("soy res desde express");
