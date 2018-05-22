e2c37dd5005048bb787c35afac1b599e15233fcc
Juan David Tabares Arce <jtabares@nisum.com>
2018-05-22 00:33:21 -0400

    updating from dell routes

--- src/client/App.js
+++ src/client/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './app.css';
 
-export default class App extends Component {
+class Greeting extends Component {
   constructor(props) {
     super(props);
     this.state = { username: null };
   }
-
   componentDidMount() {
     fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    .then(res => res.json())
+    .then(user => this.setState({ username: user.username }));
   }
-
-  render() {
+  render(){
     return (
       <div>
         {this.state.username ? (
@@ -22,6 +21,22 @@ export default class App extends Component {
           <h1>Loading.. please wait!</h1>
         )}
       </div>
+    );
+  }
+}
+
+function OtherComp(props){
+  return <h1>This is other component</h1>;
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Greeting} />
+        <Route path="/other" component={OtherComp} />
+      </Switch>
+
     );
   }
 }
