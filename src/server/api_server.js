d72cba7ecb74285e0b39d2ec6f1e3505ad8a4504
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-07-24 18:16:31 -0400

    updating

--- src/server/api_server.js
+++ src/server/api_server.js
@@ -22,7 +22,7 @@ const app = express();
 const port = process.env.PORT || 8081;
 app.use(express.static('dist'));
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
